d41e6082ae002286f23056c7482ca3d2e7851a07
thiago <160151673+Thiagolyx@users.noreply.github.com>
2024-06-14 12:34:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+/*
+var a = 10;
+var b = 15;
+
+var c = a + b;
+
+console.log(c);
+*/
+
+/*
+var a = 10;
+var b = 15;
+
+var c = a - b;
+
+console.log(c);
+*/
+/*
+var a = 10;
+var b = 15;
+
+var c = a * b;
+
+console.log(c);
+*/
